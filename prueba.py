d196007e87988b69fa64e6e99120c79bfce881d2
Pablo Marsala <pablomarsala@gmail.com>
2021-03-05 18:20:33 -0300

    Comienzo del proyecto

---
+++ prueba.py
@@ -0,0 +1,18 @@
+class Caballo:
+    def __init__(self, nombre=""):
+        self.__nombre = nombre
+
+    def set_nombre(self,nombre):
+        self.__nombre = nombre
+
+    def get_nombre(self):
+        return self.__nombre
+
+def main():
+
+    pony = Caballo("Manchas")
+    print(pony.get_nombre())
+
+
+if __name__ == "__main__":
+    main()
